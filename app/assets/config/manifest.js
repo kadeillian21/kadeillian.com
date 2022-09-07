3526d70b399a6f1839e04fefd0e8391643c75140
Kade Illian <kadeillianmt@gmail.com>
2022-09-06 20:20:59 -0600

    got js fully functioning, each page is layed out properly, including the blog page (minus the CRUD features)

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,11 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link jquery.min.js
+//= link breakpoints.min.js
+//= link browser.min.js
+//= link main.js
+//= link util.js
+//= link fontawesome-all.min.css
+
+
