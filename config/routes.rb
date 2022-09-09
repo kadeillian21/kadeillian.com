06b386264528eb3bf5687b1d6d2eefb2736af9ff
Kade Illian <kadeillianmt@gmail.com>
2022-09-09 15:19:16 -0600

    worked on desigining my blog/home page, a blog/index page, and a blog/show page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "/index" => "pages#index"
   get "/blog" => "blogs#home"
   get "/blog/all" => "blogs#index"
+  get "/blog/:id" => "blogs#show"
 
   # Authentication
   post "/sessions" => "sessions#create"
