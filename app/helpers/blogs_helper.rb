700561ba7245945e267418bdd59b5d7da4a1486f
Kade Illian <kadeillianmt@gmail.com>
2022-09-26 22:58:14 -0600

    removed unnecessary code from blog_helper

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,31 +1,20 @@
 module BlogsHelper
- def next
-  blog = Blog.find(params[:id])
-  if blog.id != Blog.last
-    blog.id + 1
-  else
-    path_to index_blog
-  end
-  return blog.id
+  def last_page
+    if @blog.id != Blog.first.id
+      blog = Blog.where.not(id: nil).where("id < ?", params[:id])
+      blog.last.id
+    else
+      blogs_path
+    end
  end
 
-def last_page
-  if @blog.id != Blog.first.id
-    blog = Blog.where.not(id: nil).where("id < ?", params[:id])
-    blog.last.id
-  else
-    blogs_path
+  def next_page
+    if @blog.id != Blog.last.id
+      blog = Blog.where.not(id: nil).where("id > ?", params[:id])
+      blog.first.id
+    else
+      blogs_path
+    end
   end
- end
-
-
- def next_page
-  if @blog.id != Blog.last.id
-    blog = Blog.where.not(id: nil).where("id > ?", params[:id])
-    blog.first.id
-  else
-    blogs_path
-  end
- end
 end
 
